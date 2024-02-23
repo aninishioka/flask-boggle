9a703cc2d929c6240feb5830d23d2041e4634072
Jack Norquist <jack.norquist@outlook.com>
2024-02-23 17:10:46 -0600

    Makes board on front end

--- static/boggle.js
+++ static/boggle.js
@@ -7,6 +7,7 @@ const $message = $(".msg");
 const $table = $("table");
 
 let gameId;
+$('.word-input-btn').on('click', newWordApi);
 
 
 /** Start */
@@ -26,9 +27,35 @@ async function start() {
 /** Display board */
 
 function displayBoard(board) {
-  // $table.empty();
+  $table.empty();
   // loop over board and create the DOM tr/td structure
+  let $tbody = $('<tbody>');
+  for (let row of board) {
+    let $tr = $('<tr>');
+    for (let cell of row) {
+      let $td = $('<td>');
+      $td.html(cell);
+      $tr.append($td);
+    }
+    $tbody.append($tr);
+  }
+  $table.append($tbody);
 }
 
+
+async function newWordApi(evt) {
+  evt.preventDefault();
+  const response = await fetch('/api/score-word', {
+    method: "POST",
+    body: JSON.stringify({ gameId: gameId, word: $wordInput.val() }),
+    headers: {
+      "content-type": "application/json",
+    }
+  });
+
+  const result = await response.json();
+  console.log(result);
+
+}
 
 start();
